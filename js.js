d98b0acd809607ed2c015c2eccffd0118c8eecc6
Calinawan_Mark <markcalinawan@gmail.com>
2024-02-26 13:50:24 +0800

    Add 2 Features

---
+++ js.js
@@ -0,0 +1,6 @@
+function updateDateTime(){
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+    document.querySelector('#datetime').textContent = currentDateTime; }
+    setInterval(updateDateTime, 1000);
+    
